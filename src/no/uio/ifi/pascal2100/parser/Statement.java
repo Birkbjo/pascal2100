baafe100ba53c67e790ab29875c933307446761f
Odd-Tørres Lunde <oddtlunde@gmail.com>
2015-10-08 17:58:08 +0200

    Statement class, fix

--- src/no/uio/ifi/pascal2100/parser/Statement.java
+++ src/no/uio/ifi/pascal2100/parser/Statement.java
@@ -3,15 +3,41 @@ package no.uio.ifi.pascal2100.parser;
 import no.uio.ifi.pascal2100.scanner.Scanner;
 
 abstract class Statement extends PascalSyntax {
-
+	
 	Statement(int n) {
 		super(n);
 		// TODO Auto-generated constructor stub
+		
 	}
 
+	
+	
 	public static Statement parse(Scanner s) {
 		// TODO Auto-generated method stub
-		return null;
+		enterParser("statement");
+		
+		Statement st = null;
+		switch (s.curToken.kind) {
+		case beginToken:
+			st = CompoundStatement.parse(s); break;
+		case ifToken:
+			st = IfStatement.parse(s); break;
+		case nameToken:
+			switch (s.nextToken.kind) {
+			case assignToken:
+			case leftBracketToken:
+				st = AssignStatement.parse(s); break;
+			default:
+				st = EmptyStatement.parse(s); break;
+			} break;
+		case whileToken:
+			st = WhileStatement.parse(s); break;
+		default:
+			st = EmptyStatement.parse(s); break;
+		}
+		
+		leaveParser("statement");
+		return st;
 	}
 
 }
