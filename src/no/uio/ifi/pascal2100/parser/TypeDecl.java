b9b8ebbc1f2e1d1893e47bbd3e66eb3673ad6492
Birkbjo <birkbjo@gmail.com>
2015-10-08 14:15:35 +0200

    Parse implementation of Type, TypeDecl and TypeDeclPart

--- src/no/uio/ifi/pascal2100/parser/TypeDecl.java
+++ src/no/uio/ifi/pascal2100/parser/TypeDecl.java
@@ -1,7 +1,10 @@
 package no.uio.ifi.pascal2100.parser;
 
-class TypeDecl extends PascalDecl {
+import no.uio.ifi.pascal2100.scanner.Scanner;
+import no.uio.ifi.pascal2100.scanner.TokenKind;
 
+class TypeDecl extends PascalDecl {
+	Type type;
 	TypeDecl(String id, int lNum) {
 		super(id, lNum);
 		// TODO Auto-generated constructor stub
@@ -17,6 +20,18 @@ class TypeDecl extends PascalDecl {
 	void prettyPrint() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public static TypeDecl parse(Scanner s) {
+		enterParser("type-decl");
+		s.test(TokenKind.nameToken);
+		TypeDecl td = new TypeDecl(s.curToken.id,s.curLineNum());
+		s.readNextToken();
+		s.skip(TokenKind.equalToken);
+		td.type = Type.parse(s);
+		s.skip(TokenKind.semicolonToken);
+		leaveParser("type-decl");
+		return td;
 	}
 
 }
