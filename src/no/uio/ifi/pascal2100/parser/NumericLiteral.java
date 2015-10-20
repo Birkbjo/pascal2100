9608961017c3b632173e97ff24ad775c362a1f8e
Birkbjo <birkbjo@gmail.com>
2015-10-20 15:03:34 +0200

    Constant prettyprints

--- src/no/uio/ifi/pascal2100/parser/NumericLiteral.java
+++ src/no/uio/ifi/pascal2100/parser/NumericLiteral.java
@@ -1,10 +1,12 @@
 package no.uio.ifi.pascal2100.parser;
 
+import no.uio.ifi.pascal2100.main.Main;
 import no.uio.ifi.pascal2100.scanner.Scanner;
+import no.uio.ifi.pascal2100.scanner.Token;
 import no.uio.ifi.pascal2100.scanner.TokenKind;
 
 public class NumericLiteral extends Constant {
-
+	Token t;
 	public NumericLiteral(int n) {
 		super(n);
 	}
@@ -16,7 +18,7 @@ public class NumericLiteral extends Constant {
 
 	@Override
 	void prettyPrint() {
-		// TODO Auto-generated method stub
+		Main.log.prettyPrint(Integer.toString(t.intVal));
 		
 	}
 	
@@ -24,6 +26,7 @@ public class NumericLiteral extends Constant {
 		enterParser("numeric-literal");
 		s.test(TokenKind.intValToken);
 		NumericLiteral nl = new NumericLiteral(s.curLineNum());
+		nl.t = s.curToken;
 		s.readNextToken();
 		
 		leaveParser("numeric-literal");
